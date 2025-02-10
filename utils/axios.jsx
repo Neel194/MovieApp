e30e1115d70a4fbbb691d5d2d43d5fd7985c7ac0
Neel194 <pneel1924@gmail.com>
2025-02-10 11:41:56 +0530

    Added tmdb api

---
+++ utils/axios.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZjEwNGQ1MWQ1NWIwOGZlNjUxZjc0ZjFlYzFkNDE4YiIsIm5iZiI6MTczOTA5NDA2NC40NzQsInN1YiI6IjY3YTg3ODMwZjVhYWE2YWZhOWUwODUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wI567sXT48yO1vKSR93JDe_5dt1wUFSBOrfyD09cwwA",
+  },
+});
+
+export default instance;

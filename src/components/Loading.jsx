9cce3a29d006a4b7594670fc36a4d108c99cc2df
Neel194 <pneel1924@gmail.com>
2025-02-12 14:08:34 +0530

    Complete loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <section className="fixed inset-0 flex justify-center items-center bg-[#1F1E24] bg-opacity-50">
+      <div className="border-t-4 border-blue-500 border-solid w-16 h-16 rounded-full animate-spin"></div>
+    </section>
+  );
+};
+
+export default Loading;

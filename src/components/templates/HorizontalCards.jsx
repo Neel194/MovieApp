2639c959d2abbd9f9df93d7131579bcefa7180e2
Neel194 <pneel1924@gmail.com>
2025-02-12 14:08:50 +0530

    Complete horizontal component

---
+++ src/components/templates/HorizontalCards.jsx
@@ -0,0 +1,28 @@
+const HorizontalCards = ({ data }) => {
+  return (
+    <div className="w-[100%] flex overflow-y-hidden mb-5 p-5">
+      {data.map((d, i) => (
+        <div key={i} className="h-[37vh] bg-zinc-900 min-w-[15%] mr-5 mb-5">
+          <img
+            className="w-full h-[55%] object-cover"
+            src={`https://image.tmdb.org/t/p/original${
+              d.backdrop_path || d.poster_path
+            }`}
+            alt=""
+          />
+          <div className="text-white p-3 h-[45%]">
+            {" "}
+            <h1 className="line-clamp-1 text-xl font-semibold ">
+              {d.title || d.name || d.original_title || d.original_name}
+            </h1>
+            <p className="">
+              {d.overview.slice(0, 40)}...
+              <span className="text-zinc-500">more</span>
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default HorizontalCards;

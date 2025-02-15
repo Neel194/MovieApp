1be99424fbefbf8169d5f6264b06425047e400d9
Neel194 <pneel1924@gmail.com>
2025-02-15 15:51:55 +0530

    Complete movie component

---
+++ src/components/Movie.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "../../utils/axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Loading from "./Loading";
+import Cards from "./templates/Cards";
+import Dropdown from "./templates/Dropdown";
+import Topnav from "./templates/Topnav";
+
+const Movie = () => {
+  document.title = "CineVerse | Movies";
+  const navigate = useNavigate();
+  const [category, setcategory] = useState("now_playing");
+  const [movie, setmovie] = useState([]);
+  const [page, setpage] = useState(1);
+  const [hasMore, sethasMore] = useState(true);
+  const GetMovie = async () => {
+    try {
+      const { data } = await axios.get(`/movie/${category}?page=${page}`);
+      if (data.results.length > 0) {
+        setmovie((prevstate) => [...prevstate, ...data.results]);
+        setpage(page + 1);
+      } else {
+        sethasMore(false);
+      }
+      // setmovie(data.results);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const refreshHandler = () => {
+    if (movie.length === 0) {
+      GetMovie();
+    } else {
+      setpage(1);
+      setmovie([]);
+      GetMovie();
+    }
+  };
+
+  useEffect(() => {
+    refreshHandler();
+  }, [category]);
+
+  return movie.length > 0 ? (
+    <div className="w-screen h-screen ">
+      <div className="px-[5%] w-full flex items-center justify-between">
+        <h1 className="text-2xl font-semibold text-zinc-400">
+          <i
+            onClick={() => navigate(-1)}
+            className="hover:text-[#6556cd] ri-arrow-left-line mr-[5%]"
+          ></i>
+          Movie
+          <small className="text-sm ml-2 text-zinc-600">({category})</small>
+        </h1>
+        <div className="flex items-center w-[80%]">
+          <Topnav />
+          <Dropdown
+            title="Category"
+            options={["popular", "top_rated", "upcoming", "now_playing"]}
+            func={(e) => setcategory(e.target.value)}
+          />
+          <div className="w-[2%]"></div>
+        </div>
+      </div>
+
+      <InfiniteScroll
+        dataLength={movie.length}
+        next={GetMovie}
+        hasMore={hasMore}
+        loader={<h1>Loading...</h1>}
+      >
+        <Cards data={movie} title={category} />
+      </InfiniteScroll>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+export default Movie;

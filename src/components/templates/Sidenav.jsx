51795b1ba1216ac78081656d8813bb3031133bcd
Neel1992004 <pneel1924@gmail.com>
2025-12-04 13:11:25 +0530

    remove repeated link components and use mapping

--- src/components/templates/Sidenav.jsx
+++ src/components/templates/Sidenav.jsx
@@ -1,47 +1,47 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { sidebarTopLinks } from '../../../utils/data/sidebarLinks';
 
 const Sidenav = () => {
-  return (
-    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
-      <h1 className="text-2xl text-white font-bold">
-        <i class="text-[#6556CD] ri-tv-fill mr-2"></i>
-        <span className="text-2xl">CineVerse</span>
-      </h1>
-      <nav className="flex flex-col text-zinc-400 text-xl">
-        <h1 className="text-white font-semibold text-xl mt-6 mb-2">
-          New Feeds
-        </h1>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="ri-fire-fill"></i> Trending
-        </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-bard-fill"></i> Popular
-        </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-movie-2-fill"></i> Movies
-        </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-tv-2-line"></i> Tv Show
-        </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-team-fill"></i> People
-        </Link>
-      </nav>
+    return (
+        <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
+            <h1 className="text-2xl text-white font-bold">
+                <i className="text-[#6556CD] ri-tv-fill mr-2"></i>
+                <span className="text-2xl">CineVerse</span>
+            </h1>
+            <nav className="flex flex-col text-zinc-400 text-xl">
+                <h1 className="text-white font-semibold text-xl mt-6 mb-2">
+                    New Feeds
+                </h1>
 
-      <hr className="border-none h-[1px] bg-zinc-400 mt-2" />
+                {sidebarTopLinks.map((item) => {
+                    return (
+                        <Link
+                            key={item.to}
+                            to={item.to}
+                            className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4"
+                        >
+                            <i className={`${item.icon} mr-2`}></i>
+                            {item.label}
+                        </Link>
+                    );
+                })}
+            </nav>
 
-      <nav className="flex flex-col text-zinc-400 text-xl ">
-        <h1 className="text-white font-semibold text-xl mt-5 mb-2">
-          Website Information
-        </h1>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-information-2-fill"></i> About CineVerse
-        </Link>
-        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
-          <i class="mr-2 ri-phone-fill"></i> Contact us
-        </Link>
-      </nav>
-    </div>
-  );
+            <hr className="border-none h-0.5 bg-zinc-400 mt-2" />
+
+            <nav className="flex flex-col text-zinc-400 text-xl ">
+                <h1 className="text-white font-semibold text-xl mt-5 mb-2">
+                    Website Information
+                </h1>
+                <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
+                    <i className="mr-2 ri-information-2-fill"></i> About
+                    CineVerse
+                </Link>
+                <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-4">
+                    <i className="mr-2 ri-phone-fill"></i> Contact us
+                </Link>
+            </nav>
+        </div>
+    );
 };
 export default Sidenav;
